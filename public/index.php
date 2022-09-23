109c9258bbfa0f3f8baccced37692e1d3dae6b96
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2022-09-23 18:44:28 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
+
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
