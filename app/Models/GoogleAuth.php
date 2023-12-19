f70ad0785ddb3fc1e37a26cab351b59004717c72
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2023-12-19 09:27:37 +0100

    Update GoogleAuth.php

--- app/Models/GoogleAuth.php
+++ app/Models/GoogleAuth.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+
 class GoogleAuth extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
